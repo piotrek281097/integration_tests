4ba063b7d71db0ea3f1f4f5af5da4ba9a8dc24f9
Piotr Plecinski <209432@edu.p.lodz.pl>
2019-06-06 01:44:14 +0200

    3.2 Testy warstwy dziedziny - adding tests in BlogManagerTest

--- blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
@@ -1,7 +1,13 @@
 package edu.iis.mto.blog.domain;
 
+import edu.iis.mto.blog.domain.errors.DomainError;
+import edu.iis.mto.blog.domain.model.BlogPost;
+import edu.iis.mto.blog.domain.model.LikePost;
+import edu.iis.mto.blog.domain.repository.BlogPostRepository;
+import edu.iis.mto.blog.domain.repository.LikePostRepository;
 import org.hamcrest.Matchers;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
@@ -18,6 +24,15 @@ import edu.iis.mto.blog.domain.repository.UserRepository;
 import edu.iis.mto.blog.mapper.BlogDataMapper;
 import edu.iis.mto.blog.services.BlogService;
 
+import javax.jws.soap.SOAPBinding;
+
+import java.util.Optional;
+
+import static com.sun.javaws.JnlpxArgs.verify;
+import static net.bytebuddy.matcher.ElementMatchers.is;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BlogManagerTest {
@@ -25,11 +40,45 @@ public class BlogManagerTest {
     @MockBean
     UserRepository userRepository;
 
+    @MockBean
+    LikePostRepository likePostRepository;
+
+    @MockBean
+    BlogPostRepository blogPostRepository;
+
     @Autowired
     BlogDataMapper dataMapper;
 
     @Autowired
     BlogService blogService;
+
+    private User userConfirmed, userNotConfirmed;
+    private BlogPost blogPost;
+
+    @Before
+    public void setup() {
+        userConfirmed = new User();
+        userConfirmed.setFirstName("Jan");
+        userConfirmed.setLastName("Klink");
+        userConfirmed.setEmail("john@domain.com");
+        userConfirmed.setAccountStatus(AccountStatus.CONFIRMED);
+        userConfirmed.setId(1L);
+        when(userRepository.findById(1L)).thenReturn(Optional.ofNullable(userConfirmed));
+
+        userNotConfirmed = new User();
+        userNotConfirmed.setFirstName("Janek");
+        userNotConfirmed.setLastName("Klinkek");
+        userNotConfirmed.setEmail("johnek@domain.com");
+        userNotConfirmed.setAccountStatus(AccountStatus.NEW);
+        userNotConfirmed.setId(2L);
+        when(userRepository.findById(2L)).thenReturn(Optional.ofNullable(userNotConfirmed));
+
+        blogPost = new BlogPost();
+        blogPost.setUser(userNotConfirmed);
+        blogPost.setEntry("blogPostEntry");
+        blogPost.setId(3L);
+        when(blogPostRepository.findById(3L)).thenReturn(Optional.ofNullable(blogPost));
+    }
 
     @Test
     public void creatingNewUserShouldSetAccountStatusToNEW() {
@@ -38,6 +87,29 @@ public class BlogManagerTest {
         Mockito.verify(userRepository).save(userParam.capture());
         User user = userParam.getValue();
         Assert.assertThat(user.getAccountStatus(), Matchers.equalTo(AccountStatus.NEW));
+    }
+
+    @Test
+    public void addingLikePostShouldBePossibleOnlyByUserWithStatusConfirmed() {
+        blogService.addLikeToPost(userConfirmed.getId(), blogPost.getId());
+        ArgumentCaptor<LikePost> likePostParam = ArgumentCaptor.forClass(LikePost.class);
+        Mockito.verify(likePostRepository).save(likePostParam.capture());
+        LikePost likePost = likePostParam.getValue();
+
+        Assert.assertThat(likePost.getUser(), Matchers.sameInstance(userConfirmed));
+        Assert.assertThat(likePost.getPost(), Matchers.sameInstance(blogPost));
+
+    }
+
+    @Test(expected = DomainError.class)
+    public void addingLikeByUserWithStatusOtherThanConfirmedShouldThrowDomainError() {
+        blogService.addLikeToPost(userNotConfirmed.getId(), blogPost.getId());
+    }
+
+    @Test(expected = DomainError.class)
+    public void addingLikeByUserWithStatusOtherThanConfirmedShouldThrowDomainErrorSecondOption() {
+        userNotConfirmed.setAccountStatus(AccountStatus.REMOVED);
+        blogService.addLikeToPost(userNotConfirmed.getId(), blogPost.getId());
     }
 
 }
